85902be561bd9d7920189aee400b92f5ca9bdcc6
Snehasish Dhar <dsnehasish74@gmail.com>
2021-03-28 13:10:52 +0530

    Add home page nav and Hero

--- frontend/src/RoutingTable.js
+++ frontend/src/RoutingTable.js
@@ -5,16 +5,18 @@ import Login from './pages/LoginPage/Login'
 import SignUp from "./pages/SignUpPage/SignUp"
 import Dashboard from "./pages/Dashboard"
 import ProductPage from "./pages/ProductPage/ProductPage"
+import Home from "./pages/Home/Home"
 
 const RoutingTable = () => {
     return (
         <Router >
             <Switch>
-                <Route exact path="/" component={ProductGeneration} />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/plastic/hdpe" component={ProductPage} />
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/signup" component={SignUp} />
                 <Route exact path="/dashboard" component={Dashboard} />
+                <Route exact path="/productgeneration" component={ProductGeneration} />
             </Switch>
         </Router >
     )
---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import Nav from './components/nav'
+import Hero from './components/hero'
+const Home = () => {
+    return (
+    <div>
+        <Nav />
+        <Hero />
+    </div>);
+}
+
+export default Home;
